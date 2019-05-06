8bc0d6445543d9dbab72f95fc9ee184afbcb20e8
Stefano Gerli <sgerli@igerli.com>
2019-05-05 18:16:02 -0600

    PCO login

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# firebase-pco-auth
+# Use PCO Sign In with Firebase
+
+This sample shows how to Sign in Firebase using [PCO](https://planning.center) on the Web.
+
+
+## Setup the sample
+
+### PCO app's creation and setup
+
+
+
+
+### Firebase app creation and setup
+
+ 1. Create a Firebase project using the [Firebase Developer Console](https://console.firebase.google.com).
+ 1. **For the web app:** Copy the Web initialisation snippet from **Firebase Console > Overview > Add Firebase to your web app** and paste it in `public/index.html` in lieu of the placeholders (where the `TODO(DEVELOPER)` is located).
+ 1. **For the server:** From the Firebase initialization snippet copy the `apiKey` value and paste it in `config.json` as the value of `firebase.apiKey` in lieu of the placeholder.
+
+Create and provide a Service Account's keys:
+ 1. Create a Service Accounts file as described in the [Server SDK setup instructions](https://firebase.google.com/docs/server/setup#add_firebase_to_your_app).
+ 1. Save the Service Account credential file as `service-account.json`
+
+Deploy your security rules:
+ 1. Run `firebase use --add` and choose your Firebase project. This will configure the Firebase CLI to use the correct
+    project locally.
+ 1. Run `firebase deploy` to deploy the security rules of the Realtime Database
+
+
+## Run the sample locally
+
+### Web
+
+You can run the sample web app locally by running `npm run start` in a console.
+
+Then open `http://localhost:8080` in your browser.
+
+Click on the **Sign in with PCO** button and a popup window will appear that will let you sign in with your Instagram account.
+
+After signing-in the app should display your name, profile pic and Instagram Pics. At this point you are authenticated in Firebase and can use the database/hosting etc...
+The app shows a simple
+
+
+## Deploy the sample
+
+You can deploy the sample's web app and server on App Engine Flex. For this:
+ - Download and setup the [gcloud](https://cloud.google.com/sdk/) CLI.
+ - Make sure Billing is enabled in your Google project and that Google Compute Instance is also enabled.
+ - Run `gcloud app deploy`
+
+ Your app will be available under your App Engine URL: `https://<project-id>.appspot.com`
+
+## License
+
+© Google, 2016. Licensed under an [Apache-2](../LICENSE) license.
---
+++ app.js
@@ -0,0 +1,220 @@
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for t`he specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+// Modules imports
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const crypto = require('crypto');
+const request = require('request');
+
+
+// Load config file
+const config = require('./config.json');
+
+const PORT = process.env.PORT || '8080'
+const HOST = '0.0.0.0'
+
+// Firebase Setup
+const admin = require('firebase-admin');
+const serviceAccount = require('./service-account.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
+});
+
+// Instagram OAuth 2 setup
+const credentials = {
+  client: {
+    id: config.pco.clientId,
+    secret: config.pco.clientSecret
+  },
+  auth: {
+    tokenHost: 'https://api.planningcenteronline.com',
+    tokenPath: '/oauth/token'
+  }
+};
+const oauth2 = require('simple-oauth2').create(credentials);
+
+// Path to the OAuth handlers.
+const OAUTH_REDIRECT_PATH = '/redirect';
+const OAUTH_CALLBACK_PATH = '/pco-callback';
+const OAUTH_MOBILE_REDIRECT_PATH = '/pco-mobile-redirect';
+const OAUTH_MOBILE_CALLBACK_PATH = '/pco-mobile-callback';
+const OAUTH_CODE_EXCHANGE_PATH = '/pco-mobile-exchange-code';
+
+// Custom URI scheme for Android and iOS apps.
+const APP_CUSTOM_SCHEME = 'pco-sign-in';
+
+// Instagram scopes requested.
+const OAUTH_SCOPES = 'people services';
+
+// ExpressJS setup
+const app = express();
+app.enable('trust proxy');
+//app.use(express.static('public'));
+app.use(cookieParser());
+
+/**
+ * Redirects the User to the Instagram authentication consent screen. Also the 'state' cookie is set for later state
+ * verification.
+ */
+app.get(OAUTH_REDIRECT_PATH, (req, res) => {
+  const state = req.cookies.state || crypto.randomBytes(20).toString('hex');
+  //console.log('Setting state cookie for verification:', state);
+  const secureCookie = req.get('host').indexOf('localhost:') !== 0;
+  //console.log('Need a secure cookie (i.e. not on localhost)?', secureCookie);
+  res.cookie('state', state, {maxAge: 3600000, secure: secureCookie, httpOnly: true});
+  const redirectUri = oauth2.authorizationCode.authorizeURL({
+    redirect_uri: `${req.protocol}://${req.get('host')}${OAUTH_CALLBACK_PATH}`,
+    scope: OAUTH_SCOPES,
+    state: state
+  });
+  //console.log('Redirecting to:', redirectUri);
+  res.redirect(redirectUri);
+});
+
+function getUserData(bearerToken) {
+  return new Promise((resolve, reject) => {
+    request('https://api.planningcenteronline.com/people/v2/me',{
+      'auth': {
+        'bearer': bearerToken,
+      },
+      'json': true 
+      }, (err, res, body) => {
+
+        if (err) { reject(err); }
+        resolve(body)
+    });
+  });
+}
+
+/**
+ * Exchanges a given Instagram auth code passed in the 'code' URL query parameter for a Firebase auth token.
+ * The request also needs to specify a 'state' query parameter which will be checked against the 'state' cookie to avoid
+ * Session Fixation attacks.
+ * This is meant to be used by Web Clients.
+ */
+app.get(OAUTH_CALLBACK_PATH, async (req, res) => {
+  //console.log('Received state cookie:', req.cookies.state);
+  //console.log('Received state query parameter:', req.query.state);
+  if (!req.cookies.state) {
+    res.status(400).send('State cookie not set or expired. Maybe you took too long to authorize. Please try again.');
+  } else if (req.cookies.state !== req.query.state) {
+    res.status(400).send('State validation failed');
+  }
+  //console.log('Received auth code:', req.query.code);
+  oauth2.authorizationCode.getToken({
+    code: req.query.code,
+    redirect_uri: `${req.protocol}://${req.get('host')}${OAUTH_CALLBACK_PATH}`
+  }).then(async results => {
+    //console.log('Auth code exchange result received:', results);
+    // We have an Instagram access token and the user identity now.
+    const accessToken = results.access_token;
+    const body = await getUserData(accessToken)
+    const pcoId = body["data"]["id"];
+    const pcoName = body["data"]["attributes"]["name"];
+    const pcoProfilePic = body["data"]["attributes"]["avatar"];
+    if (body["meta"]["parent"]["id"] != config.app.pcoOrgId) {
+      res.send("Unknown parent org!")
+    }
+    // Create a Firebase account and get the Custom Auth Token.
+    createFirebaseAccount(pcoId, pcoName, pcoProfilePic, accessToken).then(firebaseToken => {
+          // Serve an HTML page that signs the user in and updates the user profile.
+      res.redirect(config.app.loginRedirect + firebaseToken)
+    });
+    
+  });
+});
+
+/**
+ * Passes the auth code to your Mobile application by redirecting to a custom scheme URL. This serves as a fallback in
+ * case App Link/Universal Links are not supported on the device.
+ * Native Mobile apps should use this callback.
+ */
+app.get(OAUTH_MOBILE_REDIRECT_PATH, (req, res) => {
+  res.redirect(`${APP_CUSTOM_SCHEME}:/${OAUTH_MOBILE_CALLBACK_PATH}?${req.originalUrl.split('?')[1]}`);
+});
+
+/**
+ * Exchanges a given Instagram auth code passed in the 'code' URL query parameter for a Firebase auth token.
+ * This endpoint is meant to be used by native mobile clients only since no Session Fixation attacks checks are done.
+ */
+app.get(OAUTH_CODE_EXCHANGE_PATH, (req, res) => {
+  //console.log('Received auth code:', req.query.code);
+  oauth2.authCode.getToken({
+    code: req.query.code,
+    redirect_uri: `${req.protocol}://${req.get('host')}${OAUTH_MOBILE_CALLBACK_PATH}`
+  }).then(results => {
+    //console.log('Auth code exchange result received:', results);
+
+    // Create a Firebase Account and get the custom Auth Token.
+    createFirebaseAccount(results.user.id, results.user.full_name,
+        results.user.profile_picture, firebaseToken).then(firebaseToken => {
+      // Send the custom token, access token and profile data as a JSON object.
+      res.send({
+        firebaseCustomToken: firebaseToken,
+        pcoAccessToken: results.access_token,
+        photoURL: results.user.profile_picture,
+        displayName: results.user.full_name
+      });
+    });
+  });
+});
+
+
+/**
+ * Creates a Firebase account with the given user profile and returns a custom auth token allowing
+ * signing-in this account.
+ * Also saves the accessToken to the datastore at /instagramAccessToken/$uid
+ *
+ * @returns {Promise<string>} The Firebase custom auth token in a promise.
+ */
+function createFirebaseAccount(pcoId, displayName, photoURL, accessToken) {
+  // The UID we'll assign to the user.
+  const uid = `pco:${pcoId}`;
+
+  // Save the access token to the Firebase Realtime Database.
+  const databaseTask = admin.database().ref(`/pcoAccessToken/${uid}`)
+      .set(accessToken);
+
+  // Create or update the user account.
+  const userCreationTask = admin.auth().updateUser(uid, {
+    displayName: displayName,
+    photoURL: photoURL
+  }).catch(error => {
+    // If user does not exists we create it.
+    if (error.code === 'auth/user-not-found') {
+      return admin.auth().createUser({
+        uid: uid,
+        displayName: displayName,
+        photoURL: photoURL
+      });
+    }
+    throw error;
+  });
+
+  // Wait for all async task to complete then generate and return a custom auth token.
+  return Promise.all([userCreationTask, databaseTask]).then(() => {
+    // Create a Firebase custom auth token.
+    const token = admin.auth().createCustomToken(uid);
+    //console.log('Created Custom token for UID "', uid, '" Token:', token);
+    return token;
+  });
+}
+
+// Start the server
+var server = app.listen(PORT, HOST);
